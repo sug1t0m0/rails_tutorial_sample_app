48947ab36fab759c3e12ac80f6aaadfbb665b83e
sugiyama.t <f15c017d@gmail.com>
2019-10-18 09:22:06 +0900

    - セッションを使ってログイン判定

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
   end
 
   def destroy
